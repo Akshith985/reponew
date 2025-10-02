d2bc6f8fb7c492c2dae6c4ada90e8c858ac8c87e
Akshith Anand <akshithanand86@gmail.com>
2025-10-02 07:22:41 +0000

    change the welcome gif to this https://media4.giphy.com/media/v1.Y2lkPTc

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -40,15 +40,15 @@ export default function SplashScreen() {
       )}
     >
       {showWelcome ? (
-        <div className="flex flex-col items-center justify-center text-center">
+        <div className="flex items-center justify-center text-center gap-4">
             <Image 
-                src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzBjdHUzenI0OWI0YzFrcnRtNWFqOGd2b3JocXdpemxqYjlrMmZieCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/2fZufNPmTkaQiytRE3/giphy.gif"
+                src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExdTVhbHk2aGo5aWdpZHM5d2s4ZXU2eGRzaWRrb3BsYWtrOWoyM2I4dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/kNOHmVZB9eDNMXcjJT/giphy.gif"
                 alt="Welcome GIF"
-                width={300}
-                height={300}
+                width={200}
+                height={200}
                 unoptimized
             />
-            <h1 className="text-4xl font-bold font-headline mt-4">Welcome!</h1>
+            <h1 className="text-4xl font-bold font-headline">Welcome!</h1>
         </div>
       ) : (
         <div className="text-center space-y-8 max-w-lg mx-auto p-4 animate-in fade-in duration-500">
